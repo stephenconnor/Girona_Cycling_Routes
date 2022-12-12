7965fe0de3ee9ac7f374666bb482b4c26a8c06b5
Stephen Connor <steconnor@gmail.com>
2022-12-12 09:35:24 +0000

    adding long routes (2nd tab)

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -6,13 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page2.component.less']
 })
 export class Page2Component implements OnInit {
-  title = 'Page Two';
+  title = 'Long Routes';
 
   longRoutes = [
-    {title:"PAGE 2 Question 1", description:"Answer 1 description"},
-    {title:"PAGE 2 Question 2", description:"Answer 2 description"},
-    // {title:"Banyoles", description:"Short loop to Banyoles"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139069662"><p>Banyoles &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Banyoles.jpg"></a>`},
-    {title:"Page 2 Question 3", description:"Answer 3 description"}
+    {title:"St. Hilari 125", description:"Loop to St. Hilari"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139412229"><p>St Hilari 125 &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/St Hilari 125.jpg"></a>`},
+    {title:"Tossa De Mar, Coastal Route", description:"Coastal loop through Tossa De Mar & Sant Feliu de Guixols with El Angels climb to finish. "+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139523426"><p>Tossa De Mar, Coastal Route &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/TossaDeMar_Coastal.jpg"></a>`},
+    {title:"Girona → Banyoles → Rocacorba", description:"Loop from Girona to Banyoles, with the classic Rocacorba climb coming after Banyoles"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139360897"><p>Girona → Banyoles → Rocacorba &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Girona → Banyoles → Rocacorba.jpg"></a>`},
   ]
 
   constructor() { }
