9507ce95083d1c0964b8fcd2c18884bb85e314c2
Stephen Connor <steconnor@gmail.com>
2023-02-15 21:48:59 +0000

    Adding Alternative Coastal Route

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -13,6 +13,7 @@ export class Page2Component implements OnInit {
     {title:"Tossa De Mar, Coastal Route", description:"Coastal loop through Tossa De Mar & Sant Feliu de Guixols with El Angels climb to finish. "+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139523426"><p>Tossa De Mar, Coastal Route &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/TossaDeMar_Coastal.jpg"></a>`},
     {title:"Girona → Banyoles → Rocacorba", description:"Loop from Girona to Banyoles, with the classic Rocacorba climb coming after Banyoles"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139360897"><p>Girona → Banyoles → Rocacorba &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Girona → Banyoles → Rocacorba.jpg"></a>`},
     {title:"Banyoles Hills", description:"Loop from Girona to Banyoles, with a small hilly loop to finish"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139627380"><p>Banyoles Hills &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Banyoles Hills.jpg"></a>`},
+    {title:"Alternative Coastal Route", description:"Coastal loop through Calonge, Sant Feliu de Guixols & Llagostera. "+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/145987384"><p>Alternative Coastal Route &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Alternaitve_Coastal_Route.jpg"></a>`},
   ]
 
   constructor() { }
