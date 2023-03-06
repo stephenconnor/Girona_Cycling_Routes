3cf0a8b682dc69b1f743421d41e32ae2f2b736e6
Stephen Connor <steconnor@gmail.com>
2023-03-06 19:13:49 +0000

    adding Llora Constantin St Gregori route

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -15,6 +15,7 @@ export class Page1Component implements OnInit {
     {title:"Volta Puig", description:"Short loop to Adri over El Puig"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139285517"><p>Volta Puig &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p> </a><p><img src="assets/images/Volta_Puig.jpg"></p>`},
     {title:"Medinya → Cornella → Banyoles", description:"Short loop through Medinya to Banyoles"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139285865"><p>Medinya → Cornella → Banyoles &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Medinya→Cornella→Banyoles.jpg"></a>`},
     {title:"Banyoles", description:"Short loop to Banyoles"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/139069662"><p>Banyoles &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Banyoles.jpg"></a>`},
+    {title:"Llora Constantin St Gregori", description:"Llora Constantin St Gregori recovery loop. (Gravel sectors)"+`<a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/148385620"><p>Llora Constantin St Gregori &nbsp;&nbsp;<i class="bi bi-geo-alt"></i></p><img src="assets/images/Llora_Constantin_St_Gregori.jpg"></a>`},
     // Longer routes
   ]
 
