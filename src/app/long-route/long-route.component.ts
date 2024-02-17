35bc9ee746e3f619a41ce7b9b2d473babeb474bf
Stephen Connor <steconnor@gmail.com>
2024-02-17 16:12:13 +0000

    removing direct file download. garmin Connect link reinstated

--- src/app/long-route/long-route.component.ts
+++ src/app/long-route/long-route.component.ts
@@ -50,7 +50,7 @@ export class LongRouteComponent {
     },
     {
       heading: 'Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm',
-      content: 'Hilly loop to Saint Hilari Sacalm via Les Planes d\'Hostoles and Osor . <a  Title="GPX File" href="src/assets/gpx_files/longRoutes/COURSE_248797244.gpx"><p>Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm &nbsp;&nbsp;<i class="pi pi-spin pi-map-marker"></i></p><img src="assets/images/Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm.jpg"></a>'
+      content: 'Hilly loop to Saint Hilari Sacalm via Les Planes d\'Hostoles and Osor . <a role="link" Title="Garmin Connect" target="_blank" href="https://connect.garmin.com/modern/course/245544271><p>Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm &nbsp;&nbsp;<i class="pi pi-spin pi-map-marker"></i></p><img src="assets/images/Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm.jpg"></a>'
     }
     ]
 
