86baefb8ab859f99ff51a214f635b12483258402
Stephen Connor <steconnor@gmail.com>
2024-02-17 21:42:42 +0000

    gpx file path

--- src/app/long-route/long-route.component.ts
+++ src/app/long-route/long-route.component.ts
@@ -54,7 +54,7 @@ export class LongRouteComponent {
     },
     {
       heading: 'Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm',
-      content: 'Hilly loop to Saint Hilari Sacalm via Les Planes d\'Hostoles and Osor. <a href="../assets/gpx_files/longRoutes/COURSE_248797244.gpx" target="_blank" download="COURSE_248797244.gpx"><p>Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm &nbsp;&nbsp;<i class="pi pi-spin pi-map-marker"></i></p><img src="assets/images/Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm.jpg"></a>'
+      content: 'Hilly loop to Saint Hilari Sacalm via Les Planes d\'Hostoles and Osor. <a href="src/assets/gpx_files/longRoutes/COURSE_248797244.gpx" target="_blank" download="COURSE_248797244.gpx"><p>Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm &nbsp;&nbsp;<i class="pi pi-spin pi-map-marker"></i></p><img src="assets/images/Les Planes d\'Hostoles → Osor → Saint Hilari Sacalm.jpg"></a>'
     }
     ]
 
