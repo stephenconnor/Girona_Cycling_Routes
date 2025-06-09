c5a0fb0670e7db323b1d24abd1e7eb9a848c6811
Stephen Connor <steconnor@gmail.com>
2025-06-09 10:43:37 +0100

    updating angular cli 19 → 20

--- src/app/long-route/long-route.component.ts
+++ src/app/long-route/long-route.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-long-route',
-  templateUrl: './long-route.component.html',
-  styleUrls: ['./long-route.component.scss']
+    selector: 'app-long-route',
+    templateUrl: './long-route.component.html',
+    styleUrls: ['./long-route.component.scss'],
+    standalone: false
 })
 export class LongRouteComponent {
   longRoutes!: LongRouteItem[];
